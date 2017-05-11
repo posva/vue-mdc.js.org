0bb659ed6306ac8d1d4fef8bc858e128fea9f649
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-11 15:06:57 +0200

    🔧 Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,24 @@ module.exports = {
   parser: 'babel-eslint',
   env: {
     browser: true,
-    node: true
+    node: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  globals: {
+    HTMLElement: true,
   },
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
   ],
   // add your custom rules here
-  rules: {},
-  globals: {}
+  'rules': {
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 }
